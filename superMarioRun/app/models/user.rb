2c68a6c3318b124aca6926ce37ed530aaaecebee
Veronique <veronique.eldridge@gmail.com>
2014-10-23 20:43:53 +1100

    user picture upload and some layout stuff and added mario images

--- superMarioRun/app/models/user.rb
+++ superMarioRun/app/models/user.rb
@@ -20,6 +20,7 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+  mount_uploader :picture, PictureUploader
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 	has_many :runs
