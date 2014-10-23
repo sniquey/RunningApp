aba15b30096420febecc80518a21710af059d7f6
Veronique <veronique.eldridge@gmail.com>
2014-10-23 21:55:31 +1100

    fixed user picture uploads and added facebook login

--- superMarioRun/app/models/user.rb
+++ superMarioRun/app/models/user.rb
@@ -21,8 +21,29 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   mount_uploader :picture, PictureUploader
+  devise :omniauthable, :omniauth_providers => [:facebook]
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 	has_many :runs
 	belongs_to :level
+
+
+	def self.find_for_facebook_oauth(auth, signed_in_resource=nil)
+    user = User.where(:provider => auth.provider, :uid => auth.uid).first
+    if user
+      return user
+    else
+      registered_user = User.where(:email => auth.info.email).first
+      if registered_user
+        return registered_user
+      else
+        user = User.create(name:auth.extra.raw_info.name,
+                            provider:auth.provider,
+                            uid:auth.uid,
+                            email:auth.info.email,
+                            password:Devise.friendly_token[0,20],
+                          )
+      end
+  	end
+  end 
 end
--- superMarioRun/config/initializers/devise.rb
+++ superMarioRun/config/initializers/devise.rb
@@ -256,4 +256,6 @@ Devise.setup do |config|
   # When using omniauth, Devise cannot automatically set Omniauth path,
   # so you need to do it manually. For the users scope, it would be:
   # config.omniauth_path_prefix = '/my_engine/users/auth'
+  require "omniauth-facebook"
+  config.omniauth :facebook, "587390321365898", "4dba8c7c3d47136f513ee001b93c14cf"
 end
