8bb80fd44e98410fe65b2137b174cab204f4d5d9
jadefeng <j.feng.1991@gmail.com>
2014-10-24 13:50:43 +1100

    Trying to save each run into an entry attached to a run id

--- superMarioRun/app/assets/javascripts/locations.js
+++ superMarioRun/app/assets/javascripts/locations.js
@@ -5,36 +5,61 @@ $(document).ready(function () {
 				return; // timer is already running.
 			}
 			getLocation();
-			locationtracking = setInterval(getLocation, 1000);
-			console.log('setInterval returned', locationtracking)
+			locationtracking = setInterval(getLocation, 5000);
+			// console.log('setInterval returned', locationtracking)
 		});
 		
-		$('#stoptracking').on('click', function () {
-			console.log('stopping', locationtracking)
+		$('#stoptracking').on('click', function (lat_long_time_object) {
+			console.log('stopping', locationtracking);
 			clearInterval(locationtracking);
+			// reset the counter and the location tracking
 			locationtracking = null;
+			counter = 0;
 		});
 
 });
 
-function getLocation () {
+var lat_long_time_object;
+var counter = 0;
+var lat;
+var lon; 
+var datetime;
+
+function getLocation (lat_long_time_object, counter) {
 	// Check to see if the browser supports the GeoLocation API.
 	if (navigator.geolocation) {
 		console.log("Got your location.");
 		// Get the location
 		var position;
 		navigator.geolocation.getCurrentPosition(function(position) {
-			var lat = position.coords.latitude;
-			var lon = position.coords.longitude;
+			// Find the latitude, longitude and time of the GPS call
+			counter += 1;
+			lat = position.coords.latitude;
+			lon = position.coords.longitude;
+			
+			var currentdate = new Date(); 
+			datetime = currentdate.getDate() + "/"
+			                + (currentdate.getMonth()+1)  + "/" 
+			                + currentdate.getFullYear() + " @ "  
+			                + currentdate.getHours() + ":"  
+			                + currentdate.getMinutes() + ":" 
+			                + currentdate.getSeconds();
 			console.log("Latitude: "+ lat);
 			console.log("Longitude: "+lon);
+			console.log("Time is " + datetime);
+			// Saving this into the counter
+			// lat_long_time_object[counter] = {
+			// 	latitude: lat,
+			// 	longitude: lon,
+			// 	time: datetime
+			// }; 
 			// Show the map
 			showMap(lat, lon);
 		});
 		} else {
 	  // Print out a message to the user.
 	  // document.write('Your browser does not support GeoLocation');
-	  console.log("Fuck. Your browser does not have Geolocation");
+	  console.log("Your browser does not have Geolocation");
 	}
 }
 
@@ -53,7 +78,7 @@ function showMap(lat, lon) {
   };
 
   // Generate the Map
-  var map = new google.maps.Map($('#map'), mapOptions);
+  var map = new google.maps.Map($('.map'), mapOptions);
 
   // Add a Marker to the Map
   var marker = new google.maps.Marker({
