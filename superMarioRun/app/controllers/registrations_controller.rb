86024a39b281852b8ad106e64bef60246099c687
Veronique <veronique.eldridge@gmail.com>
2014-10-25 14:15:25 +1100

    rachet set up views

--- superMarioRun/app/controllers/registrations_controller.rb
+++ superMarioRun/app/controllers/registrations_controller.rb
@@ -48,7 +48,7 @@ class RegistrationsController < Devise::RegistrationsController
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up) do |u|
-      u.permit(:first_name, :last_name, :email, :password, :password_confirmation)
+      u.permit(:name, :email, :password, :password_confirmation)
     end
     devise_parameter_sanitizer.for(:account_update) do |u|
       u.permit(:first_name, :last_name, :email, :password, :password_confirmation)
