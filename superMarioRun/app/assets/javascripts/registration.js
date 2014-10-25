d6e854bac690eb7634db1b22cdd222e8649a2f3a
Veronique <veronique.eldridge@gmail.com>
2014-10-25 19:44:30 +1100

    further mobile set up and settings

--- superMarioRun/app/assets/javascripts/registration.js
+++ superMarioRun/app/assets/javascripts/registration.js
@@ -1,4 +1,5 @@
 $(document).ready( function () {
+	$("#registrationpart2").hide()
 
 
 });
--- superMarioRun/config/routes.rb
+++ superMarioRun/config/routes.rb
@@ -2,15 +2,15 @@ Rails.application.routes.draw do
 
   root :to => 'pages#home'
 
-  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
+  #devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
 
   # get '/dashboard' => 'home#dashboard', :as => 'user_root'
 
-	# devise_for :user do
- #   		get "/login", :to => "devise/sessions#new" # Add a custom sign in route for user sign in
- #   		get "/logout", :to => "devise/sessions#destroy" # Add a custom sing out route for user sign out
- #   		get "/register", :to => "devise/registrations#new" # Add a Custom Route for Registrations
-	# end
+	devise_for :user do
+   		get "/login", :to => "devise/sessions#new" # Add a custom sign in route for user sign in
+   		get "/logout", :to => "devise/sessions#destroy" # Add a custom sing out route for user sign out
+   		get "/register", :to => "devise/registrations#new" # Add a Custom Route for Registrations
+	end
 
   resources :locations
 
