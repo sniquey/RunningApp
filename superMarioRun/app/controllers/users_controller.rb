8ea3089a6a621ec2828047aa28c11c228dfe852a
jadefeng <j.feng.1991@gmail.com>
2014-10-26 13:49:02 +1100

    Added seed data to levels

--- superMarioRun/app/controllers/users_controller.rb
+++ superMarioRun/app/controllers/users_controller.rb
@@ -26,9 +26,11 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     @user.level_id = 1
-    @user.lives = 3
-    @user.runs_per_week = 1
-    
+    @user.total_lives = 3
+    # unless @user.runs_per_week
+    #   @user.runs_per_week = 1
+    # end
+
     respond_to do |format|
       if @user.save
         format.html { redirect_to @user, notice: 'User was successfully created.' }
@@ -72,6 +74,6 @@ class UsersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
-      params.require(:user).permit(:name, :email, :password_digest, :avatar, :height, :dob, :runs_per_week, :lives)
+      params.require(:user).permit(:name, :email, :password_digest, :avatar, :height, :dob, :runs_per_week)
     end
 end
--- superMarioRun/db/seeds.rb
+++ superMarioRun/db/seeds.rb
@@ -5,3 +5,34 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Level.destroy_all
+
+level1 = Level.create(:name => 'Level 1', :coin_freq => 1, :mushroom_freq => 10, :turtle_freq => 100, 
+				:description => 'Level 1 Run', :coin_threshold => 0)
+level2 = Level.create(:name => 'Level 2', :coin_freq => 2, :mushroom_freq => 20, :turtle_freq => 200, 
+				:description => 'Level 2 Run', :coin_threshold => 0)
+level3 = Level.create(:name => 'Level 3', :coin_freq => 3, :mushroom_freq => 30, :turtle_freq => 300, 
+				:description => 'Level 3 Run', :coin_threshold => 0)
+level4 = Level.create(:name => 'Level 4', :coin_freq => 4, :mushroom_freq => 40, :turtle_freq => 400, 
+				:description => 'Level 4 Run', :coin_threshold => 0)
+level5 = Level.create(:name => 'Level 5', :coin_freq => 5, :mushroom_freq => 50, :turtle_freq => 500, 
+				:description => 'Level 5 Run', :coin_threshold => 0)
+level6 = Level.create(:name => 'Level 6', :coin_freq => 6, :mushroom_freq => 60, :turtle_freq => 600, 
+				:description => 'Level 6 Run', :coin_threshold => 0)
+level7 = Level.create(:name => 'Level 7', :coin_freq => 7, :mushroom_freq => 70, :turtle_freq => 700, 
+				:description => 'Level 7 Run', :coin_threshold => 0)
+level8 = Level.create(:name => 'Level 8', :coin_freq => 8, :mushroom_freq => 80, :turtle_freq => 800, 
+				:description => 'Level 8 Run', :coin_threshold => 0)
+
+#  coin_freq      :float
+#  mushroom_freq  :float
+#  turtle_freq    :float
+#  name           :string(255)
+#  description    :text
+#  coin_threshold :integer
+#  image          :text
+#  theme          :string(255)
+#  type           :string(255)
+#  created_at     :datetime
+#  updated_at     :datetime
