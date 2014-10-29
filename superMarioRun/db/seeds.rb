42bd3cefa7d4f32324b3916629562639d3b293f1
Veronique <veronique.eldridge@gmail.com>
2014-10-29 14:56:01 +1100

    seed

--- superMarioRun/db/seeds.rb
+++ superMarioRun/db/seeds.rb
@@ -13,19 +13,19 @@ Level.destroy_all
 level1 = Level.create(:id =>1, :name => 'Level 1', :coin_freq => 10, :mushroom_freq => 25, :turtle_freq => 100, 
 				:description => 'Level 1 Run', :coin_threshold => 0, :theme => 'Clouds')
 level2 = Level.create(:id => 2, :name => 'Level 2', :coin_freq => 2, :mushroom_freq => 20, :turtle_freq => 200, 
-				:description => 'Level 2 Run', :coin_threshold => 100)
+				:description => 'Level 2 Run', :coin_threshold => 100, :theme => 'Garden')
 level3 = Level.create(:id => 3, :name => 'Level 3', :coin_freq => 3, :mushroom_freq => 30, :turtle_freq => 300, 
-				:description => 'Level 3 Run', :coin_threshold => 200)
+				:description => 'Level 3 Run', :coin_threshold => 200, :theme => 'Hell')
 level4 = Level.create(:id => 4, :name => 'Level 4', :coin_freq => 4, :mushroom_freq => 40, :turtle_freq => 400, 
-				:description => 'Level 4 Run', :coin_threshold => 300)
+				:description => 'Level 4 Run', :coin_threshold => 300, :theme => '')
 level5 = Level.create(:id => 5, :name => 'Level 5', :coin_freq => 5, :mushroom_freq => 50, :turtle_freq => 500, 
-				:description => 'Level 5 Run', :coin_threshold => 400)
+				:description => 'Level 5 Run', :coin_threshold => 400, :theme => '')
 level6 = Level.create(:id => 6, :name => 'Level 6', :coin_freq => 6, :mushroom_freq => 60, :turtle_freq => 600, 
-				:description => 'Level 6 Run', :coin_threshold => 500)
+				:description => 'Level 6 Run', :coin_threshold => 500, :theme => '')
 level7 = Level.create(:id => 7, :name => 'Level 7', :coin_freq => 7, :mushroom_freq => 70, :turtle_freq => 700, 
-				:description => 'Level 7 Run', :coin_threshold => 600)
+				:description => 'Level 7 Run', :coin_threshold => 600, :theme => '')
 level8 = Level.create(:id => 8, :name => 'Level 8', :coin_freq => 8, :mushroom_freq => 80, :turtle_freq => 800, 
-				:description => 'Level 8 Run', :coin_threshold => 700)
+				:description => 'Level 8 Run', :coin_threshold => 700, :theme => '')
 
 #  coin_freq      :float
 #  mushroom_freq  :float
