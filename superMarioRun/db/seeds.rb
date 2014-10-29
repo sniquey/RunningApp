e4c348b6c8dd226bae527c961804332ca030d979
Veronique <veronique.eldridge@gmail.com>
2014-10-29 11:48:05 +1100

    levels style and progress bar

--- superMarioRun/db/seeds.rb
+++ superMarioRun/db/seeds.rb
@@ -11,7 +11,7 @@ Level.destroy_all
 
 ## These will be pushed in
 level1 = Level.create(:id =>1, :name => 'Level 1', :coin_freq => 10, :mushroom_freq => 25, :turtle_freq => 100, 
-				:description => 'Level 1 Run', :coin_threshold => 0)
+				:description => 'Level 1 Run', :coin_threshold => 0, :theme => 'Clouds')
 level2 = Level.create(:id => 2, :name => 'Level 2', :coin_freq => 2, :mushroom_freq => 20, :turtle_freq => 200, 
 				:description => 'Level 2 Run', :coin_threshold => 100)
 level3 = Level.create(:id => 3, :name => 'Level 3', :coin_freq => 3, :mushroom_freq => 30, :turtle_freq => 300, 
